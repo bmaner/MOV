36a6c77b1c1eccff66aedba66c9b12b8570446c5
bmaner <bmanerdaniel@gmail.com>
2022-05-20 16:30:01 +0900

    Fix modules in serverside

--- pages/api/contact/index.js
+++ pages/api/contact/index.js
@@ -1,11 +1,11 @@
-import withHandler from '@/libs/server/withHandler'
-import client from '@/libs/server/client'
+import withHandler from "../../../libs/server/withHandler";
+import client from "../../../libs/server/client";
 
 async function handler(req, res) {
   const {
     body: { name, email, description, url },
-  } = req
-  if (req.method === 'POST') {
+  } = req;
+  if (req.method === "POST") {
     const request = await client.request.create({
       data: {
         name,
@@ -13,12 +13,16 @@ async function handler(req, res) {
         description,
         url,
       },
-    })
+    });
     res.json({
       ok: true,
       request,
-    })
+    });
   }
 }
 
-export default withHandler({ methods: ['POST'], fn: handler, isPrivate: false })
+export default withHandler({
+  methods: ["POST"],
+  fn: handler,
+  isPrivate: false,
+});
--- pages/api/messages/index.js
+++ pages/api/messages/index.js
@@ -1,36 +1,36 @@
-import withHandler from '@/libs/server/withHandler'
-import client from '@/libs/server/client'
+import withHandler from "../../../libs/server/withHandler";
+import client from "../../../libs/server/client";
 
 async function handler(req, res) {
   const {
     body: { message },
-  } = req
+  } = req;
 
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     const messages = await client.message.findMany({
       skip: 0,
       take: 10,
-      orderBy: [{ createdAt: 'desc' }],
-    })
+      orderBy: [{ createdAt: "desc" }],
+    });
     res.json({
       ok: true,
       messages,
-    })
-  } else if (req.method === 'POST') {
+    });
+  } else if (req.method === "POST") {
     const createdMessage = await client.message.create({
       data: {
         message,
       },
-    })
+    });
     res.json({
       ok: true,
       createdMessage,
-    })
+    });
   }
 }
 
 export default withHandler({
-  methods: ['GET', 'POST'],
+  methods: ["GET", "POST"],
   fn: handler,
   isPrivate: false,
-})
+});
